a2d0e9ccb90de0c3d75a84709d09ca02bc72880f
NebulaEclipse <chrisdouglas200516@gmail.com>
2024-03-26 09:33:05 -0400

    Added schema.sql and seeds.sql

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2');
 require('dotenv').config();
+const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: process.DB.USERNAME,
-    password: process.DB.PASSWORD,
-    database: process.DB.NAME
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 module.exports = db;
